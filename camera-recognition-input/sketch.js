7965edc5c97fb76eb524be2e6e3c2f5525e9ddc1
lexiboi <hi@alexpetercharles.ch>
2021-06-08 21:33:54 +0200

    final hand recognition

--- camera-recognition-input/sketch.js
+++ camera-recognition-input/sketch.js
@@ -1,38 +1,89 @@
-// f = first
-const container = document.getElementsByClassName('first')[0];  // container div element
+const container = document.getElementsByClassName('sketch')[0];  // container div element
+
+// draw time
+let seconds = 5;
+
+// point Size
+const minSize = 1;
+const maxSize = 80;
+let pointSize = 1;
+
+// fist recognition pixel deltas
+const deltaHand = 200; // open hand = high delta
+const deltaFist = -200;
+// map fist recognition to point Size
+const scale = (input) => 
+  (input - deltaHand) * (maxSize - minSize) / (deltaFist - deltaHand) + minSize;
+
 let state;
-
-const camera = document.getElementsByClassName('camera')[0];
-if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then((stream) => {
-      camera.srcObject = stream;
-    });
-}
-
-let predictions = [];
-const loaded = () => { console.log('loaded'); }
+let hand;
+let timeLeft;
 
 const firstSketch = ( p ) => {
   p.setup = () => {
     p.createCanvas(container.offsetWidth, container.offsetHeight);
     state = p.createGraphics(container.offsetWidth, container.offsetHeight);
 
-    const handpose = ml5.handpose(camera, loaded);
-    handpose.on('predict', results => {
-      predictions = results;
-    });
+    camera = p.createCapture(p.VIDEO);
+    camera.size(container.offsetWidth, container.offsetHeight);
+    camera.hide();
+
+    const handpose = ml5.handpose(camera, { flipHorizontal: true }, () => { console.log('model loaded') });
+    handpose.on('predict', results => { hand = results[0]; });
+
+    const restartButton = document.getElementsByClassName('restart')[0];
+    restartButton.addEventListener(('click'), () => {
+      p.clear();
+      state.clear();
+      setCountdown();
+    })
+    setCountdown();
   };
+
+  const setCountdown = () => {
+    timeLeft = seconds;
+    const countdown = setInterval(() => {
+      if(timeLeft <= 0) { clearInterval(countdown); return; } 
+      timeLeft -= 1;
+    }, 1000);
+  }
+
+  const setPointSize = () => {
+    if(hand) {
+      // fist detection with only ringFinger
+      const ringLast = hand.annotations.ringFinger[0];
+      const ringFirst = hand.annotations.ringFinger[3];
+      const delta = ringLast[1] - ringFirst[1];
+      pointSize = scale(delta);
+      // console.log(delta)
+    }
+  }
+
+  const drawHand = () => {
+    setPointSize()
+    if(hand) {
+      hand.landmarks.forEach(point => {
+        state.fill(0);
+        state.noStroke();
+        state.ellipse(point[0], point[1], pointSize, pointSize)
+      });
+    }
+  }
+
+  const drawTime = () => {
+    const counter = document.getElementsByClassName('counter')[0];
+    counter.children[0].textContent = timeLeft;
+  }
   
 
   p.draw = () => {
     state.noStroke();
-    state.fill(0);
-    state.rect(50, 400, 100, 100);
-
+    state.fill(255);
     p.image(state, 0, 0);
 
-    // if(p.frameCount % 10 === 0) { console.log(predictions) }
+    drawTime();
+
+    if(timeLeft > 0) drawHand();
   };
 };
 
